59efb4b6cf2b3900e20517d1a23a0c1c382eb1db
Tannin <sherb@gmx.net>
2016-06-04 20:27:22 +0200

    translation

---
+++ src/installer_quick_en.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>InstallerQuick</name>
+    <message>
+        <location filename="installerquick.cpp" line="35"/>
+        <source>Installer for very simple archives</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SimpleInstallDialog</name>
+    <message>
+        <location filename="simpleinstalldialog.ui" line="14"/>
+        <source>Quick Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="simpleinstalldialog.ui" line="22"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="simpleinstalldialog.ui" line="53"/>
+        <location filename="simpleinstalldialog.ui" line="56"/>
+        <source>Opens a Dialog that allows custom modifications.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="simpleinstalldialog.ui" line="59"/>
+        <source>Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="simpleinstalldialog.ui" line="66"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="simpleinstalldialog.ui" line="76"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
